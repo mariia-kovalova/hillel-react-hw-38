cce5936eb84b4f8b5b154e81d568c063c81426bf
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-01-19 15:22:13 +0200

    prettier

--- src/components/AddUserForm/AddUserForm.jsx
+++ src/components/AddUserForm/AddUserForm.jsx
@@ -1,26 +1,35 @@
+import { Formik } from "formik";
+
 // import { useState } from "react";
 
 export function AddUserForm() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, username, phone } = event.target.elements;
+  };
+
   return (
     <div className="container">
-      <form>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" required />
-        </div>
-        <div>
-          <label htmlFor="lastName">User name</label>
-          <input type="text" id="lastName" required />
-        </div>
-        <div>
-          <label htmlFor="pnone">Pnone number</label>
-          <input type="text" id="pnone" required />
-        </div>
-        <div className="grid">
-          <button type="submit">Save</button>
-          <button type="button">Cancel</button>
-        </div>
-      </form>
+      <Formik>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" required />
+          </div>
+          <div>
+            <label htmlFor="lastName">User name</label>
+            <input type="text" name="username" id="lastName" required />
+          </div>
+          <div>
+            <label htmlFor="pnone">Pnone number</label>
+            <input type="text" name="phone" id="pnone" required />
+          </div>
+          <div className="grid">
+            <button type="submit">Save</button>
+            <button type="button">Cancel</button>
+          </div>
+        </form>
+      </Formik>
     </div>
   );
 }

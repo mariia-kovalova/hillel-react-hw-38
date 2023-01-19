5624994ecda86d663527d890cd14c5e3f81cf4da
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-01-20 00:21:24 +0200

    modal window

--- src/components/AddUserForm/AddUserForm.jsx
+++ src/components/AddUserForm/AddUserForm.jsx
@@ -1,35 +1,72 @@
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
-// import { useState } from "react";
-
-export function AddUserForm() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const { name, username, phone } = event.target.elements;
+export function AddUserForm({ setModalOpen }) {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log('values', values);
+    resetForm();
+    setModalOpen(false);
   };
+
+  const initialValues = {
+    name: '',
+    username: '',
+    phone: '',
+  };
+
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+    username: yup.string().required(),
+    phone: yup.string().required(),
+  });
+
+  const Error = styled(ErrorMessage)`
+    color: red;
+    margin-bottom: 10px;
+  `;
 
   return (
     <div className="container">
-      <Formik>
-        <form onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" required />
+            <Field type="text" name="name" id="name" />
+            <Error component="div" name="name" />
           </div>
           <div>
             <label htmlFor="lastName">User name</label>
-            <input type="text" name="username" id="lastName" required />
+            <Field type="text" name="username" id="lastName" />
+            <Error component="div" name="username" />
           </div>
           <div>
             <label htmlFor="pnone">Pnone number</label>
-            <input type="text" name="phone" id="pnone" required />
+            <Field type="text" name="phone" id="pnone" />
+            <Error component="div" name="phone" />
           </div>
           <div className="grid">
             <button type="submit">Save</button>
-            <button type="button">Cancel</button>
+            <button
+              type="button"
+              onClick={() => {
+                setModalOpen(false);
+              }}
+            >
+              Cancel
+            </button>
           </div>
-        </form>
+        </Form>
       </Formik>
     </div>
   );
 }
+
+AddUserForm.prototype = {
+  setModalOpen: PropTypes.func.isRequired,
+};
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { UsersList } from "../UsersList/UsersList";
-import { AddUserForm } from "../AddUserForm/AddUserForm";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { UsersList } from '../UsersList/UsersList';
+import { ModalWindow } from '../Modal/Modal';
+import { AddUserForm } from '../AddUserForm/AddUserForm';
 
-const FETCH_URL = "https://jsonplaceholder.typicode.com/users";
+const FETCH_URL = 'https://jsonplaceholder.typicode.com/users';
 
 export function App() {
   const [users, setUsers] = useState([]);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
-    axios
-     .get(FETCH_URL)
-     .then((res) => {
-        setUsers(res.data);
-      });
-  }, [])
+    axios.get(FETCH_URL).then(res => {
+      setUsers(res.data);
+    });
+  }, []);
 
   function deleteUserById(userId) {
-    const newUsers = users.filter(({id}) => id !== userId);
+    const newUsers = users.filter(({ id }) => id !== userId);
     setUsers(newUsers);
   }
 
   return (
     <>
-    <UsersList usersInfo={users} onDelete={deleteUserById}/>
-    <AddUserForm/>
+      <UsersList usersInfo={users} onDelete={deleteUserById} />
+      <button onClick={() => setModalOpen(true)}>Add a user</button>
+      <ModalWindow isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+        <AddUserForm setModalOpen={setModalOpen} />
+      </ModalWindow>
     </>
-    
   );
 }

6cba5a1cdc661de388381978fb2d844e0bed6952
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-03-17 17:32:21 +0200

    api upg

--- src/api/JP-Service.js
+++ src/api/JP-Service.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 
 export const getUsers = async () => {
-  const { data } = await axios.get('/users', {
-    params: {},
-  });
+  const { data } = await axios.get('/users');
 
   return data;
 };
